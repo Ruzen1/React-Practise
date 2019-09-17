379021c9f8f723299add215a16b2d5c5e1ef46b5
Ruzen1 <shakya.rujen@ebisuinc.jp>
2019-09-17 18:02:43 +0900

    test

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function formatName(user){
+    return user.firstname + '' + user.lastName;
+}
+
+const user = {
+    firstname:'Harper',
+    lastname:'Perez'
+};
+
+const element = (
+    <h1>
+        Hello,{formatName(user)}!
+    </h1>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
