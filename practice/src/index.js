f34317225179ee3a3c1c1c75c33402f4e4d3aa80
Ruzen1 <shakya.rujen@ebisuinc.jp>
2019-09-20 16:39:13 +0900

    Composing Components

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Welcome(props) {
-    return <h1>Hello,{props.name}</h1>
+
+function Welcome(props){
+    return <h1>Hello, {props.name}</h1>;
 }
 
-const element = <Welcome name = "Sara" />;
-ReactDOM.render(
-    element,
+function App(){
+    return (
+        <div>
+            <Welcome name = "Sara" />
+            <Welcome name = "Cahal" />
+            <Welcome name = "Edite" />
+        </div>
+    );
+}
+
+ReactDOM.render (
+    <App />,
     document.getElementById('root')
-)
+);
