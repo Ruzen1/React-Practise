95dd0658422179a755b13d33161d199738b45fc2
Ruzen1 <shakya.rujen@ebisuinc.jp>
2019-09-20 16:27:50 +0900

    timeInterval

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,21 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user){
-    return user.firstname + ' ' + user.lastname;
+function tick(){
+    const element =(
+        <div>
+            <h1>Hello,world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element,document.getElementById('root'));
 }
 
-const user = {
-    firstname:'Harper',
-    lastname:'Perez',
-};
-
-const element = <div tabIndex ="1">
-    <h1>Hello!</h1>
-    <h2>Good to see you here</h2>
-</div>;
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+setInterval(tick,2);
