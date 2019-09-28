1d0624c158f4ae1dd1c9cda085da010fa16b2c58
Ruzen1 <shakya.rujen@ebisuinc.jp>
2019-09-29 01:41:40 +0900

    State and Lifecycle

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,47 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
 
-function Comment(props){
-    return (
-        <div className = "Comment">
-            <div className = "UserInfo">
-                <img className = "Avatar"
-                    src = {props.author.avatarUrl}
-                    alt={props.author.name}/>
-                    <div className ="UserInfo-name">
-                        {props.author.name}
-                    </div>
-            </div>
-            <div className="Comment-text">
-                {props.text}
-                </div>
-                <div className ="Comment-date">
-                    {formatDate(props.date)}
-                </div>
+    componentDidMount(){
+        this.timerID=setInterval(
+            () => this.tick(),
+            5
+        );
+    }
 
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date:new Date()
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
+        );
+    }
+}
+
+
+    ReactDOM.render(
+        <Clock />,
+        document.getElementById('root')
     );
-}
 
-function formatDate(date){
-    return date.toLocaleTimeString();
-}
 
-const comment ={
-    date:new Date(),
-    text : 'I hope you enjoy learning React!',
-    author:{
-        name:'Hello Kitty',
-        avatarUrl:'https://placekitten.com/g/64/64',
-    },
-};
-
-ReactDOM.render (
-    <Comment 
-    date = {comment.date}
-    text = {comment.text}
-    author = {comment.author}
-    />,
-    document.getElementById('root')
-);
